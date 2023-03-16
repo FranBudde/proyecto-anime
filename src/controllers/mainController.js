00d88c49aede11c51ca229add58127a9876f5cc7
FranBudde <buddemeyerizzo@gmail.com>
2023-03-16 19:21:30 -0300

    funcionando el home con la base de datos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,16 @@
 const path = require ('path');
 const fs = require ('fs');
 
+const animesFilePath = path.join(__dirname, '../data/animes-data.json');
+
+function getAnimes(){
+	return JSON.parse(fs.readFileSync(animesFilePath, 'utf-8'));
+} //esta funcion permite actualizar la lista de products para cada accion del CRUD.
+
 const mainController = {
     index : (req, res)=>{
-        res.render('index')
+        const animes = getAnimes();
+        res.render('index',{animes})
     }
 }
 
