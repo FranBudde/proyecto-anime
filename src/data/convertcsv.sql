8a0fbfa15425ab38a009074cd9a02182fc7a9d0b
FranBudde <buddemeyerizzo@gmail.com>
2023-04-19 22:41:18 -0300

    DB

---
+++ src/data/convertcsv.sql
@@ -0,0 +1,12 @@
+CREATE TABLE mytable(
+   id       BIT  NOT NULL PRIMARY KEY
+  ,name     VARCHAR(6) NOT NULL
+  ,resumen  VARCHAR(7) NOT NULL
+  ,generos0 VARCHAR(6) NOT NULL
+  ,ranking  NUMERIC(3,1) NOT NULL
+  ,imagen   VARCHAR(10) NOT NULL
+);
+INSERT INTO mytable(id,name,resumen,generos0,ranking,imagen) VALUES (1,'naruto','Resumne','isekai',4.6,'naruto.jpg');
+INSERT INTO mytable(id,name,resumen,generos0,ranking,imagen) VALUES (1,'naruto','Resumne','isekai',4.6,'naruto.jpg');
+INSERT INTO mytable(id,name,resumen,generos0,ranking,imagen) VALUES (1,'naruto','Resumne','isekai',4.6,'naruto.jpg');
+INSERT INTO mytable(id,name,resumen,generos0,ranking,imagen) VALUES (1,'naruto','Resumne','isekai',4.6,'naruto.jpg');
